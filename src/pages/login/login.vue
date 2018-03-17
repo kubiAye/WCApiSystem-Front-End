4df8c98fe215e70f386af29a19b23b0dd064edda
Hy <526029942@qq.com>
2018-03-17 23:41:47 +0800

    初始化项目

---
+++ src/pages/login/login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="login">
+      <!--<img class="bg" src="https://img1.tiancitycdn.com/eve/showpic/EVE_Rubicon_Wallpaper_1920x1080.jpg">-->
+    <div class="iconfont icon-event" style="font-size: 40px;color: #000"></div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        methods: {},
+        computed: {},
+        watch: {},
+
+    }
+</script>
+<style scoped lang="sass" rel="stylesheet/stylus">
+  #login
+    height: 100%
+    width: 100%
+    .bg
+      height: 100%
+      width: 100%
+
+</style>
---
+++ src/utils/requestUtil.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import Vue from 'vue'
+
+let vue = new Vue()
+/*初始化配置*/
+axios.create({
+
+})
